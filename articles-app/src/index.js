c714cc772c39cf122bfd3d6a1bc217dc39a87ef3
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-07-26 13:20:43 +0300

    store + defining app components

--- articles-app/src/index.js
+++ articles-app/src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Switch, Route } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
+
+import "./assets/index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { store, history } from "./redux/store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
